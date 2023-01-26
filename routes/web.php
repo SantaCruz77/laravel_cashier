7c09dde579ebeef4e9f5e50f85f80a298014b3be
SantaCruz77 <steveyamada66@gmail.com>
2023-01-26 09:40:52 +0900

    setting JavaScript & Environment variable

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,9 +19,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/* ----------サブスクリプション決済---------- */
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+/* Route::get('/dashboard', function() {
+    /* auth()->user()->subscription('default')->swap('price_1MTyugGGqLVGSXrfjMud05hP'); */
+    /* return view('dashboard');
+})->middleware(['auth'])->name('dashboard'); */
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -31,5 +39,39 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 Route::get('/subscription', function() {
-    return view('subscription');
+    return view('subscription', [
+        'intent' => auth()->user()->createSetupIntent()
+    ]);
 })->middleware(['auth'])->name('subscription');
+
+Route::post('/user/subscribe', function (Request $request) {
+    $request->user()->newSubscription('default', 'price_1MTyugGGqLVGSXrfCqsEQSkY')
+    /* ->trialDays(10) */
+    ->create($request->paymentMethodId);
+
+    return redirect('/dashboard');
+
+})->middleware(['auth'])->name('subscribe.post');
+
+Route::get('/basic', function() {
+    return view('basic');
+})->middleware(['auth', 'basic'])->name('basic');
+
+Route::get('/premium', function() {
+    return view('premium');
+})->middleware(['auth', 'premium'])->name('premium');
+
+/* ----------Single Charges決済---------- */
+
+Route::get('/purchase', function() {
+    return view('purchase');
+})->middleware(['auth'])->name('purchase');
+
+Route::post('/purchase', function(Request $request) {
+    $request->user()->charge(
+        100, $request->paymentMethodId
+    );
+
+    return redirect('/dashboard');
+
+})->middleware(['auth'])->name('purchase.post');
